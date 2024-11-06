7bb731d9877f1446dc008768281a433081d2ffb1
ahmedyousry74 <ayousry750@gmail.com>
2024-11-06 22:04:01 +0200

    fix

--- src/components/landing/Home/HomeAboutUs.tsx
+++ src/components/landing/Home/HomeAboutUs.tsx
@@ -2,6 +2,12 @@
 import React from 'react'
 import { SingleHero } from "../shared"
 import img from '@/assets/images/landing/about_1.png'
+import hero from '@/assets/images/hero-img.webp'
+import list from '@/assets/images/list.svg'
+import book from '@/assets/images/book.svg'
+import global from '@/assets/images/global.svg'
+import idea from '@/assets/images/idea.svg'
+import shape from '@/assets/images/shape-2.svg'
 import right from '@/assets/images/check2.png'
 import Image from "next/image"
 import imgBg from '@/assets/images/landing/about_8.png'
@@ -16,7 +22,7 @@ const HomeAboutUs = ({ data }: { data: HomeAboutUsIN }) => {
   const t = useTranslations();
 
   return (
-    <section className="bg-[#fff] py-12 about_us_section">
+    <section className="bg-[#fff] py-12 about_us_section relative">
       <RevealAnimation >
 
 
@@ -26,37 +32,44 @@ const HomeAboutUs = ({ data }: { data: HomeAboutUsIN }) => {
         <div className='flex justify-center items-center flex-row bg_bg'>
             <span className="  " > {t('about_us')} </span>
         </div>
-          <SingleHero
-            title={data?.[0]?.title}
-            subTitle={data?.[0]?.details || ''}
-            img={data?.[0]?.image || img.src}
-            imgBg={imgBg}
-          >
-            {!!data?.items?.data?.length && (
-
-              <div className="flex flex-col gap-4 text-lg">
-                {data?.items?.data?.map((item, index) => (
-                  <div key={index} className="flex items-center gap-2">
-                    <div className='icon__  flex justify-center items-center flex-row rounded-[4px] p-[3px]'>
-                    
-                    <Image alt="check" src={right} width={30} height={30} />
-                    </div>
-                    {/* <div className='icon__ bg-secondary flex justify-center items-center flex-row rounded-[4px] p-[3px] border border-solid border-[#ccc]'>
-                    
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M20 6L9 17L4 12" stroke="#533AD4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
-                    </div> */}
-                    <p> {item?.detail_ar}</p>
-                  </div>
-                ))}
-              </div>
-            )}
-
-            <div className="profile__btn mt-8 mb-5">
-              <a href="" target='_blank' className='bg-primary font-medium text-[17px] py-[8px] px-[35px] text-[#fff] rounded-full'> ملف الأعمال </a>
+          <div className='text-center قثمشفهرث md:text-start grid grid-cols-1 md:grid-cols-2 lg:grid-cols-12 gap-6 lg:gap-8 w-full items-center  '>
+            <div className='lg:col-span-7 relative z-10 header__hero__desc flex justify-start items-start flex-col gap-4'>
+              <h1 className=''>منصة الفكر العلمي</h1>
+              <h2>هي منصة <span>تعليمية</span> متطورة تهدف إلى تحسين وتطوير عملية التعليم</h2>
+              <p> من خلال تقديم حلول مبتكرة تجمع بين التكنولوجيا والمحتوى الأكاديمي تسعى المنصة إلى تلبية احتياجات الطلاب وأعضاء هيئة التدريس على حدٍ سواء من خلال تقديم خدمات متكاملة تسهم في تعزيز جودة التعليم وتحقيق أقصى استفادة من الموارد التعليمية من خلال تحويل المقررات الدراسية التقليدية إلى كتب إلكترونية تفاعلية وتوفير نظام إدارة تعلم  LMSمتكامل للمؤسسات التعليمية تسعى المنصة إلى تعزيز التجربة التعليمية للطلاب وأعضاء هيئة التدريس على حد سواء عبر استخدام تقنيات حديثة تُمكّنهم من الوصول إلى محتوى تعليمي ثري ومتاح في أي وقت ومن أي مكان. </p>
+              <ul className='flex justify-start itesm-start flex-col gap-3 mt-5'>
+                <li className='flex justify-start itesm-start flex-row gap-2'>  
+                  <Image src={list} alt="list" />
+                  <span> المكتبة الأساسية للفكر العلمي ومكتبات الجامعات مثل جامعة الملك عبد العزيز </span>
+                </li>
+                <li className='flex justify-start itesm-start flex-row gap-2'>  
+                  <Image src={list} alt="list" />
+                  <span> تركز المنصة على توفير قارئ ذكي بخصائص متقدمة </span>
+                </li>
+                <li className='flex justify-start itesm-start flex-row gap-2'>  
+                  <Image src={list} alt="list" />
+                  <span> المكتبة الأساسية للفكر العلمي ومكتبات الجامعات مثل جامعة الملك عبد العزيز </span>
+                </li>
+              </ul>
             </div>
-          </SingleHero>
+            <div className="lg:col-span-5">
+              <div className='hero__img relative'>
+                <Image src={hero} alt='hero img' className='object-contain z-9 relative' />
+              </div>
+            </div>
+            <div className="book bounce">
+              <Image alt="book" src={book} />
+            </div>
+            <div className="global rotate">
+              <Image alt="global" src={global} />
+            </div>
+            <div className="shape moveHorizontal">
+              <Image alt="shape" src={shape} />
+            </div>
+            <div className="idea">
+              <Image alt="idea" src={idea} />
+            </div>
+          </div>
         </div>
 
       </RevealAnimation>
