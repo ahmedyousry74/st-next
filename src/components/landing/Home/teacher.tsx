86eeaac4a020d0048e2b7bbc79102260ca2abf86
ahmedyousry74 <ayousry750@gmail.com>
2024-11-20 14:02:21 +0200

    change layout

--- src/components/landing/Home/teacher.tsx
+++ src/components/landing/Home/teacher.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 import { SingleHero } from "../shared"
 
 import teacherImg from '@/assets/images/teacher.svg'
+import teacher from '@/assets/images/teacher.jpg'
+import student from '@/assets/images/student.jpg'
 
 import right from '@/assets/images/check2.png'
 import { ArchiveBook, Book1, Data2, PresentionChart, Setting5, TickCircle, UserEdit } from "iconsax-react"
@@ -24,7 +26,7 @@ const Hometeacher = () => {
     <section className="py-12 teacher__section relative bg-primary-color-dark">
         <div className="md:px-[40px] px-[25px]">
             <div className='flex justify-center items-center flex-row md:gap-[40px] gap-[25px] lg:flex-nowrap flex-wrap'>
-                <div className="features__one flex justify-center items-center flex-col gap-4 px-4">
+                {/* <div className="features__one flex justify-center items-center flex-col gap-4 px-4">
                     <h2 className='text-white font-semibold text-[20px] text-center' > منصة الفكر العلمي للحلول التعليمية أعضاء هيئة التدريس لوحة تحكم </h2>
                     <Link href={'/teacher'} className="my-5">
                         <button className="bg-third-color-dark font-medium text-[17px] !py-[10px] !px-[35px] text-[#fff] rounded-full cursor-pointer "> اقرأ المزيد </button>
@@ -35,6 +37,30 @@ const Hometeacher = () => {
                     <Link href={'/students'} className="my-5">
                         <button className="bg-third-color-dark font-medium text-[17px] !py-[10px] !px-[35px] text-[#fff] rounded-full cursor-pointer "> اقرأ المزيد </button>
                     </Link>
+                </div> */}
+                <div className='flex justify-start items-start flex-col features__'>
+                    <div className='IMG__ w-full'>
+                        <Image src={teacher} alt="teacher" className='w-full' />
+                    </div>
+                    <div className='Desc__ bg-white py-[15px] px-[25px] flex justify-start items-start flex-col gap-4'>
+                        <h3 className='text-primary-color-dark text-[24px] font-semibold'> أعضاء هيئة التدريس </h3>
+                        <p className='text-primary-color-dark text-[15px] font-normal'> منصة الفكر العلمي للحلول التعليمية أعضاء هيئة التدريس لوحة تحكم  </p>
+                        <Link href={'/teacher'}>
+                            <button className="bg-third-color-dark font-medium text-[17px] !py-[10px] !px-[35px] text-[#fff] rounded-full cursor-pointer "> اقرأ المزيد </button>
+                        </Link>
+                    </div>
+                </div>
+                <div className='flex justify-start items-start flex-col features__'>
+                    <div className='IMG__ !w-[457px] !h-[305px]'>
+                        <Image src={student} alt="teacher" className='!w-full h-full' />
+                    </div>
+                    <div className='Desc__ bg-white py-[15px] px-[25px] flex justify-start items-start flex-col gap-4'>
+                        <h3 className='text-primary-color-dark text-[24px] font-semibold'> الطلاب</h3>
+                        <p className='text-primary-color-dark text-[15px] font-normal'> تقدم المنصة محتوى تعليمي للطلاب شامل يناسب جميع المستويات   </p>
+                        <Link href={'/students'}>
+                            <button className="bg-third-color-dark font-medium text-[17px] !py-[10px] !px-[35px] text-[#fff] rounded-full cursor-pointer "> اقرأ المزيد </button>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
